946b61206459ff84ae2f620d6794e951b9751d86
Rick Neeft <rick@chroomsoft.nl>
2020-10-25 22:42:48 +0100

    website Dutch nl

--- SRC/WEB/build.ps1
+++ SRC/WEB/build.ps1
@@ -1,13 +1,26 @@
-$template = Get-Content -Path 'template_NL' -Raw
+$template = Get-Content -Path 'template_NL.html' -Raw
 $nl_items = Get-ChildItem ('NL\*.html');
-$imgs = Get-ChildItem('IMG\*.*');
 
-Copy-Item "IMG" -Destination "BIN\NL\IMG" -Recurse
+# making sure folder exists
+mkdir -PATH "BIN\IMG"  -Force
+mkdir -PATH "BIN\css" -Force
 
+# copying site items
+Copy-Item "IMG\*" -Destination "BIN\img" -Force
+Copy-Item "css\*" -Destination "BIN\css" -Force
+Copy-Item "favicon.ico" -Destination "BIN\favicon.ico" -Force
+
+## replace all items for Dutch.
 foreach ($item in $nl_items) {
-    $path = "BIN\NL\"  + $item.Name;
+    $path = "BIN\nl\"  + $item.Name;
     $content = Get-Content -Path $item -Raw
     $newContent = $template -replace "{{Content}}" , $content
     $newContent = $newContent -replace "{{Name}}", $item.Name
     New-Item -Path $path -Value $newContent -Force
 }
+
+# create index html (Dutch)
+$content = Get-Content -Path "BIN\nl\index.html" -Raw
+# replace all ../ in the index.html
+$newContent = $content -replace "\.\.\/", ""
+New-item "BIN\index.html" -Value $newContent -Force
