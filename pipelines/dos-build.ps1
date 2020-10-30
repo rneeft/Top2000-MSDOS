4940a34a9b760412d76107793a254a6ddaf60195
Rick Neeft <rick@chroomsoft.nl>
2020-10-30 01:20:10 +0100

    added Wait-Process in dos-build

--- pipelines/dos-build.ps1
+++ pipelines/dos-build.ps1
@@ -3,5 +3,7 @@ $conf = Get-Content -Path 'pipelines\dosbox.conf' -Raw
 $newContent = $conf -replace "{{Source_Folder}}" , $env:SOURCEFOLDER
 New-Item "pipelines\dosbox.conf" -Value $newContent -Force
 
-SET SDL_VIDEODRIVER=dummy
-& "C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe" -conf $env:SOURCEFOLDER\pipelines\dosbox.conf -noconsole
+Set-Variable SDL_VIDEODRIVER=dummy
+$dos = Start-Process -FilePath "C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe"  -ArgumentList "-conf $env:SOURCEFOLDER\pipelines\dosbox.conf -noconsole" -PassThru
+
+Wait-Process $dos.Id
