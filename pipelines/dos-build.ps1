f0469cd0538ed638795c1c657f1191885f93be5e
Rick Neeft <rick@chroomsoft.nl>
2020-10-30 16:44:28 +0100

    clean up

--- pipelines/dos-build.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host $env:SOURCEFOLDER
-$conf = Get-Content -Path 'pipelines\dosbox.conf' -Raw
-$newContent = $conf -replace "{{Source_Folder}}" , $env:SOURCEFOLDER
-New-Item "pipelines\dosbox.conf" -Value $newContent -Force
-
-Set-Variable SDL_VIDEODRIVER=dummy
-$dos = Start-Process -FilePath "C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe"  -ArgumentList "-conf $env:SOURCEFOLDER\pipelines\dosbox.conf -noconsole" -PassThru
-
-Wait-Process $dos.Id
--- pipelines/runit.bat
+++
@@ -1,2 +0,0 @@
-SET SDL_VIDEODRIVER=dummy
-"C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe" -conf dosbox.conf -noconsole
